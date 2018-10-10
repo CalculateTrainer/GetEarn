8d3ec6ef09842d9cbf9a0f156ca746a70b2298d3
Tiger <tiger@tigerdemacbook-pro.local>
2018-10-10 20:45:17 +0800

    project

---
+++ NetEarn/NetEarn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetEarn
+//
+//  Created by Tiger on 10/10/18.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
